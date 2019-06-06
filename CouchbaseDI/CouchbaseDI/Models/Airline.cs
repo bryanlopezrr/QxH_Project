e688b6f2f15b9f522e9d45ceeac29cd42df01ab5
Robert Leatherman <r.leatherman82@gmail.com>
2019-06-06 15:00:13 -0400

    MVC with Couchbase

---
+++ CouchbaseDI/CouchbaseDI/Models/Airline.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CouchbaseDI.Models
+{
+    public class Airline
+    {
+        public string Id { get; set; }
+        //META(a).id, a.name, a.country, a.callsign
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string Callsign { get; set; }
+    }
+}
