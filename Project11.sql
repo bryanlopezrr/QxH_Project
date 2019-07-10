9b546d5e8576ee20031b4d86ad5d2507e7201a34
Senthuren Sivarupan <44373495+Senthuren99@users.noreply.github.com>
2019-07-10 08:11:23 -0400

    Add files via upload

---
+++ Project11.sql
@@ -0,0 +1,44 @@
+CREATE TABLE USA(
+    SEQ INT,
+    ITEM_ID INT,
+    Description VARCHAR,
+    Orders INT,
+    Sold INT,
+    Sold_Today INT,
+    AFS INT,
+    Start INT,
+    Amount DECIMAL,
+    New INT,
+    P_Mins INT,
+    A_Mins INT
+    );
+    
+CREATE TABLE JPN
+ SEQ INT,
+    ITEM_ID INT,
+    Description VARCHAR,
+    Orders INT,
+    Sold INT,
+    Sold_Today INT,
+    AFS INT,
+    Start INT,
+    Amount DECIMAL,
+    New INT,
+    P_Mins INT,
+    A_Mins INT
+    );
+
+CREATE TABLE EUROPE
+ SEQ INT,
+    ITEM_ID INT,
+    Description VARCHAR,
+    Orders INT,
+    Sold INT,
+    Sold_Today INT,
+    AFS INT,
+    Start INT,
+    Amount DECIMAL,
+    New INT,
+    P_Mins INT,
+    A_Mins INT
+    );
