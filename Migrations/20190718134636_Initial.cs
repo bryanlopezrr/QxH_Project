794e2d2d0d9d46b6fb1b162c5dc949b870880668
rleath82 <38876176+rleath82@users.noreply.github.com>
2019-07-22 10:49:35 -0400

    Add files via upload

---
+++ Migrations/20190718134636_Initial.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QxHOracleLoader.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    PlanSeqId = table.Column<int>(nullable: false),
+                    ItemDescription = table.Column<string>(nullable: true),
+                    OrderQuantity = table.Column<int>(nullable: false),
+                    OrderSldTdy = table.Column<int>(nullable: false),
+                    PlannedMinutesQty = table.Column<int>(nullable: false),
+                    ActualMinutesQty = table.Column<int>(nullable: false),
+                    NetworkId = table.Column<int>(nullable: false),
+                    CompanyId = table.Column<int>(nullable: false),
+                    AvaiForSaleQty = table.Column<int>(nullable: false),
+                    ShowDate = table.Column<string>(nullable: true),
+                    ShowCd = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.ItemId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
