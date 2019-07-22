1d83c602430d89be861444d724f6bb3937c842d1
Robert Leatherman <r.leatherman82@gmail.com>
2019-07-22 10:45:53 -0400

    Loader Program Added

---
+++ QxHOracleLoader/QxHOracleLoader/Data/LoaderContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace QxHOracleLoader.Models
+{
+    public class LoaderContext : DbContext
+    {
+        public LoaderContext (DbContextOptions<LoaderContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Item { get; set; }
+    }
+}
---
+++ QxHOracleLoader/QxHOracleLoader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QxHOracleLoader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
